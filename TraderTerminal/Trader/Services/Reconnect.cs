c3a76fd2f01385b37201ee4f9931c17a5a34c675
Alexandr Chumakov <Alex1234014@gmail.com>
2025-02-11 20:55:52 +0500

    -Moved the reconnection policy for Wc and Rest clients to a separate class

---
+++ TraderTerminal/Trader/Services/Reconnect.cs
@@ -0,0 +1,32 @@
+using System.Net.WebSockets;
+using Polly;
+using Polly.Retry;
+using RestSharp;
+
+namespace Trader.Services;
+
+public static class Reconnect
+{
+    public static AsyncRetryPolicy<RestResponse> GetRestPolicy()
+    {
+        return Policy.Handle<HttpRequestException>().OrResult<RestResponse>(r =>
+                !r.IsSuccessful && (int)r.StatusCode >= 500 && (int)r.StatusCode < 600)
+            .WaitAndRetryAsync(retryCount: 3,
+                sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)));
+    }
+
+    public static AsyncRetryPolicy GetWcPolicy()
+    {
+        return Policy
+            .Handle<WebSocketException>()
+            .Or<Exception>()
+            .WaitAndRetryAsync(
+                retryCount: 5,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // Экспоненциальная задержка
+                onRetry: (exception, timespan, retryAttempt, context) =>
+                {
+                    Console.WriteLine($"[BitfinexWsClient] Ошибка подключения: {exception.Message}. Попытка {retryAttempt}. Повтор через {timespan.TotalSeconds}с.");
+                }
+            );
+    }
+}
