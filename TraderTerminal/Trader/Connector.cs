e54a61f90fb6b006f72e768845a0084037abc036
Alexandr Chumakov <Alex1234014@gmail.com>
2025-02-12 15:14:58 +0500

    - Completed the logic for processing incoming events and unsubscribing at the maximum count

--- TraderTerminal/Trader/Connector.cs
+++ TraderTerminal/Trader/Connector.cs
@@ -3,14 +3,32 @@ using Trader.Models;
 
 namespace Trader;
 
-public class Connector(IRestClient restClient) : IConnector
+public class Connector : IConnector
 {
+    private readonly IWsClient _wsClient;
+    private readonly IRestClient _restClient;
+    private readonly Dictionary<string, int> _tradesCounter = new();
+    private readonly Dictionary<string, int> _tradesMaxCounter = new();
+
+    private readonly Dictionary<string, int> _candelesCounter = new();
+    private readonly Dictionary<string, long?> _candelesMaxCounter = new();
+
+    public Connector(IWsClient wsClient, IRestClient restClient)
+    {
+        _restClient = restClient;
+        _wsClient = wsClient;
+
+        _wsClient.TradeReceived += OnTradeReceived;
+        _wsClient.CandleReceived += OnCandleReceived;
+    }
+
+
     public Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount) =>
-        restClient.GetNewTradesAsync(pair, maxCount);
+        _restClient.GetNewTradesAsync(pair, maxCount);
 
     public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from,
         long? count = 0, DateTimeOffset? to = null) =>
-        restClient.GetCandleSeriesAsync(pair, periodInSec, from, count, to);
+        _restClient.GetCandleSeriesAsync(pair, periodInSec, from, count, to);
 
 
     public event Action<Trade>? NewBuyTrade;
@@ -18,10 +36,18 @@ public class Connector(IRestClient restClient) : IConnector
 
     public void SubscribeTrades(string pair, int maxCount = 100)
     {
+        _tradesCounter[pair] = 0;
+        _tradesMaxCounter[pair] = maxCount;
+
+        _wsClient.SubscribeTrades(pair);
     }
 
     public void UnsubscribeTrades(string pair)
     {
+        _tradesCounter.Remove(pair);
+        _tradesMaxCounter.Remove(pair);
+
+        _wsClient.UnsubscribeTrades(pair);
     }
 
     public event Action<Candle>? CandleSeriesProcessing;
@@ -29,9 +55,44 @@ public class Connector(IRestClient restClient) : IConnector
     public void SubscribeCandles(string pair, int periodInSec,
         long? count = 0, DateTimeOffset? from = null, DateTimeOffset? to = null)
     {
+        _candelesCounter[pair] = 0;
+        _candelesMaxCounter[pair] = count;
     }
 
     public void UnsubscribeCandles(string pair)
     {
+        _candelesCounter.Remove(pair);
+        _candelesMaxCounter.Remove(pair);
+
+        _wsClient.UnsubscribeCandles(pair);
+    }
+
+    private void OnCandleReceived(Candle candle)
+    {
+        if (!_candelesCounter.ContainsKey(candle.Pair)) return;
+
+        _candelesCounter[candle.Pair]++;
+
+        if (_candelesCounter[candle.Pair] <= _candelesMaxCounter[candle.Pair])
+            CandleSeriesProcessing?.Invoke(candle);
+        else
+            UnsubscribeCandles(candle.Pair);
+    }
+
+    private void OnTradeReceived(Trade trade)
+    {
+        if (!_tradesCounter.ContainsKey(trade.Pair)) return;
+
+        _tradesCounter[trade.Pair]++;
+
+        if (_tradesCounter[trade.Pair] <= _tradesMaxCounter[trade.Pair])
+        {
+            if (trade.Side == "buy")
+                NewBuyTrade?.Invoke(trade);
+            else
+                NewSellTrade?.Invoke(trade);
+        }
+        else
+            UnsubscribeTrades(trade.Pair);
     }
 }
